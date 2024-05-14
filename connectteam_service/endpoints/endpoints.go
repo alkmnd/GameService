e6a8b3db5446eb51dbb3556323a5e9a750be15ad
alkmnd <merch47@bwg-io.site>
2024-05-14 16:53:43 +0300

    Change endpoint for save results

--- connectteam_service/endpoints/endpoints.go
+++ connectteam_service/endpoints/endpoints.go
@@ -5,7 +5,7 @@ const (
 	GetGameURL           = BaseURL + "/api/games/{id}"
 	StartGameURL         = BaseURL + "/api/games/start/{id}"
 	EndGameURL           = BaseURL + "/api/games/end/{id}"
-	SaveResultsURL       = BaseURL + "/api/games/results/{id}"
+	SaveResultsURL       = BaseURL + "/api/games/{id}/results"
 	GetUserByIdURL       = BaseURL + "/api/users/{id}"
 	GetUserActivePlanURL = BaseURL + "/api/users/{id}/plan"
 	GetTopicWithIdURL    = BaseURL + "/api/topics/{id}"
