e409832955e25b159c9c2801b19a28066704d512
2cubes <order@2cubes.ru>
2024-07-04 16:04:26 +1000

    Fixes

--- repository/endpoints/endpoints.go
+++ repository/endpoints/endpoints.go
@@ -1,7 +1,7 @@
 package endpoints
 
 const (
-	BaseURL              = "http://localhost:8001"
+	BaseURL              = "http://host.docker.internal:8001"
 	GetGameURL           = BaseURL + "/api/games/{id}"
 	StartGameURL         = BaseURL + "/api/games/start/{id}"
 	EndGameURL           = BaseURL + "/api/games/end/{id}"
