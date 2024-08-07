d6dc3106df309d80a7a9a26fff7a71b15fafbf2c
alkmnd <merch47@bwg-io.site>
2024-08-07 19:51:26 +0300

    Fix bags

--- repository/models/results.go
+++ repository/models/results.go
@@ -3,6 +3,14 @@ package models
 import "github.com/google/uuid"
 
 type Rates struct {
+	Value           int         `json:"value"`
+	Tags            []uuid.UUID `json:"tags"`
+	UserId          uuid.UUID   `json:"user_id"`
+	UserTemporaryId uuid.UUID   `json:"user_temp_id"`
+	Name            string      `json:"name"`
+}
+
+type Result struct {
 	Value           int       `json:"value"`
 	Tags            []Tag     `json:"tags"`
 	UserId          uuid.UUID `json:"user_id"`
@@ -11,5 +19,5 @@ type Rates struct {
 }
 
 type GetResultsResponse struct {
-	Results []Rates `json:"results"`
+	Results []Result `json:"results"`
 }
