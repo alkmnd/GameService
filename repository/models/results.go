a38b5568dea02bf92dff183f45ba2aeab2aad163
alkmnd <merch47@bwg-io.site>
2024-08-07 14:25:53 +0300

    Add temporary id

--- repository/models/results.go
+++ repository/models/results.go
@@ -3,15 +3,17 @@ package models
 import "github.com/google/uuid"
 
 type Rates struct {
-	Value  int         `json:"value"`
-	Tags   []uuid.UUID `json:"tags"`
-	UserId uuid.UUID   `json:"user_id"`
-	Name   string      `json:"name"`
+	Value           int         `json:"value"`
+	Tags            []uuid.UUID `json:"tags"`
+	UserId          uuid.UUID   `json:"user_id"`
+	UserTemporaryId uuid.UUID   `json:"user_temp_id"`
+	Name            string      `json:"name"`
 }
 
 type GetResultsResponse struct {
-	Value  int       `json:"value"`
-	Tags   []Tag     `json:"tags"`
-	UserId uuid.UUID `json:"user_id"`
-	Name   string    `json:"name"`
+	Value           int       `json:"value"`
+	Tags            []Tag     `json:"tags"`
+	UserId          uuid.UUID `json:"user_id"`
+	UserTemporaryId uuid.UUID `json:"user_temp_id"`
+	Name            string    `json:"name"`
 }
